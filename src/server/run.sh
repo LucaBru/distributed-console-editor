b89f3c83a6cad7268d38173e5f23b31c902718af
Ezan777 <enrizangri@gmail.com>
2025-04-22 18:12:45 +0200

    test6

--- src/server/run.sh
+++ src/server/run.sh
@@ -16,15 +16,15 @@ clear
 
 go run . --raft_bootstrap --raft_id=nodeA --address=https://server1.enricozangrando.com --raft_data_dir /tmp/my-raft-cluster &
 sleep 2 &&
-go run . --raft_id=nodeB --https://server2.enricozangrando.com --raft_data_dir /tmp/my-raft-cluster &
+go run . --raft_id=nodeB --address=https://server2.enricozangrando.com --raft_data_dir /tmp/my-raft-cluster &
 sleep 2 &&
 go run . --raft_id=nodeC --address=https://server3.enricozangrando.com --raft_data_dir /tmp/my-raft-cluster &
 sleep 2
 
 go install github.com/Jille/raftadmin/cmd/raftadmin@latest
 echo -e "\nAdding nodes B and C to the cluster" 
-raftadmin https://server1.enricozangrando.com add_voter nodeB 127.0.0.1:50052 0
-raftadmin --leader multi:///https://server1.enricozangrando.com,127.0.0.1:50052 add_voter nodeC https://server3.enricozangrando.com 0
+raftadmin https://server1.enricozangrando.com add_voter nodeB https://server2.enricozangrando.com 0
+raftadmin --leader multi:///https://server1.enricozangrando.com,https://server2.enricozangrando.com add_voter nodeC https://server3.enricozangrando.com 0
 sleep 2
 
 echo -e "\nCluster is online 🚀🚀"
